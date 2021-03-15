da4e2b5b271caa525c57ddfd5bc135b2d564d59a
Rodrigo Alier <rodricastro44@gmail.com>
2021-03-15 12:58:02 +0100

    Mi primer commit

---
+++ src/controlador/Main.java
@@ -0,0 +1,15 @@
+package controlador;
+
+import modelo.Producto;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Producto tomate=new Producto("tomate", 0.56);
+		
+		
+	}
+
+}
